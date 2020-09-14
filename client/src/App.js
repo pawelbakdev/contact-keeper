8a2ce66af60debe568a4244a7a5d702d022f213d
pawel-bak-dev <pawel.bak.dev@gmail.com>
2020-09-14 15:56:35 +0200

    Register & Login Forms

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import './App.css';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Navbar from './components/layout/Navbar';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 const App = () => {
   return (
@@ -18,6 +20,8 @@ const App = () => {
               <Switch>
                 <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
+                <Route exact path='/register' component={Register} />
+                <Route exact path='/login' component={Login} />
               </Switch>
             </div>
           </Fragment>
