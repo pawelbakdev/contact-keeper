e68a2096455f9eb1a737762112ccae3b9957c8b6
pawel-bak-dev <pawel.bak.dev@gmail.com>
2020-09-16 21:10:40 +0200

    Delete Contacts

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -7,10 +7,10 @@ const ContactItem = ({ contact }) => {
     ContactContext
   );
 
-  const { id, name, email, phone, type } = contact;
+  const { _id, name, email, phone, type } = contact;
 
   const onDelete = () => {
-    deleteContact(id);
+    deleteContact(_id);
     clearCurrent();
   };
 
